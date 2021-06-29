90e5732ae6352a4cbefa75190ee6113fbc55bcc5
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-06-29 19:27:11 +0200

    adds deploy script, fixes error reporting

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+BUILD_ARG=$1
+SERVER="sgc"  # ssh config name or full adress
+
+rustup override set nightly
+cross build --target=aarch64-unknown-linux-gnu $BUILD_ARG
+rsync -vh --progress \
+  target/aarch64-unknown-linux-gnu/debug/sensor_central \
+  $SERVER:/tmp/
+
+cmds="
+sudo mv /tmp/sensor_central /home/ha/sensor_central
+sudo chown ha:ha /home/ha/sensor_central
+sudo systemctl restart sens.service
+"
+
+ssh -t sgc "$cmds"
