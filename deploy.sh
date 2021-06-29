faa7e37e46d4986655a45d882dd1016d80e8a855
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-06-29 21:05:24 +0200

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,16 @@ set -e
 
 BUILD_ARG=$1
 SERVER="sgc"  # ssh config name or full adress
+RELEASE=debug
+
+if [[ BUILD_ARG == "--release" ]]; then
+	RELEASE=release
+fi
 
 rustup override set nightly
 cross build --target=aarch64-unknown-linux-gnu $BUILD_ARG
 rsync -vh --progress \
-  target/aarch64-unknown-linux-gnu/debug/sensor_central \
+  target/aarch64-unknown-linux-gnu/$RELEASE/sensor_central \
   $SERVER:/tmp/
 
 cmds="
